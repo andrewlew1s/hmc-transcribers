cecaf5e1afbadbd41c83e5232146bf1425eb4146
garrettcheadle <gcheadle@g.hmc.edu>
2019-04-29 18:47:59 -0700

    linted and cleaned app.py file

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS, cross_origin
-import pyrebase
-import pytesseract
-import flair
-from flair.models import SequenceTagger
-from flair.data import Label, Token, Sentence
-import torch
-from PIL import Image
 import requests
+from PIL import Image
+from flask import Flask, jsonify, request
+from flask_restful import Api
+from flask_cors import CORS, cross_origin
+from flair.models import SequenceTagger
+from flair.data import Sentence
+import pytesseract
+import pyrebase
 
-app = Flask(__name__)
-api = Api(app)
-#cors = CORS(app, resources={r"/transcribe": {"origins": "ec2-13-57-16-89.us-west-1.compute.amazonaws.com:8000"}})
-CORS(app)
+APP = Flask(__name__)
+API = Api(APP)
+CORS(APP)
 
-config = {
+#Configuration information for Firebase connection
+CONFIG = {
     "apiKey": "AIzaSyCrQrzpGmwxtICzplBfV4kFw02CYPYHxdc",
     "authDomain": "hmc-transcribers.firebaseapp.com",
     "databaseURL": "https://hmc-transcribers.firebaseio.com",
@@ -24,49 +22,41 @@ config = {
     "messagingSenderId": "859954806298"
 }
 
-firebase = pyrebase.initialize_app(config)
-storage = firebase.storage()
-database = firebase.database()
+#Create References to Firebase Cloud Storage
+FIREBASE = pyrebase.initialize_app(CONFIG)
+STORAGE = FIREBASE.storage()
+DATABASE = FIREBASE.database()
 
-# modelURL = storage.child("models/final-model.pt").get_url(None)
-model = SequenceTagger.load_from_file("final-model.pt")
+#Load in Text Clasfication Model
+TEXT_MODEL = SequenceTagger.load_from_file("gurt-model.pt")
 
-def downloadImage(imageName):
-    
-    # storage.child("cards/classexnew.jpg").download("downloaded.jpg")
-    imageURL = storage.child("cards/"+ imageName).get_url(None)
-    # image = urllib.request.urlretrieve(imageURL, '001.jpg')
-    
-    # file = urllib.request.urlretrieve(imageURL, '001.jpg')
-    img = Image.open(requests.get(imageURL, stream=True).raw)
+def download_image(image_name):
+    '''
+    Returns image from firebase storage.
+    '''
+    image_url = STORAGE.child("cards/"+ image_name).get_url(None)
+    img = Image.open(requests.get(image_url, stream=True).raw)
     return img
 
-# def push_data():
-#     if request.method == 'POST':
-
-# image = downloadImage()
-
-
-@app.route('/')
+@APP.route('/')
 def testing():
+    '''
+    Home route for testing purposes.
+    '''
     return 'It works!'
 
-
-# UI accesses to generated fields
-@app.route('/fields', methods=['POST','GET'])
-def get_data():
-    if request.method == 'GET':
-        return jsonify(data)
-
-
-
-def run_OCR(image):
+def run_ocr(image):
+    '''
+    Runs OCR on given image and returns output with no
+    newline, comma, or bar character. If OCR returns no
+    output, then return that image is unreadable.
+    '''
     # Define config parameters
     configs = ('-l eng --oem 1 --psm 3')
     # Run tesseract OCR on image
-    outputText = pytesseract.image_to_string(image, config=configs)
+    output_text = pytesseract.image_to_string(image, config=configs)
     text = ''
-    for i, char in enumerate(outputText):
+    for i, char in enumerate(output_text):
         if char == '\n' or char == ',' or char == '|':
             text += ' '
         else:
@@ -74,40 +64,44 @@ def run_OCR(image):
     readable = True
     try:
         text = Sentence(text)
-    except Exception as e:
-        print(e)
+    except Exception as ocr_error:
+        print(ocr_error)
         readable = False
     return text, readable
 
-@app.route('/transcribe', methods=['POST', 'GET'])
+@APP.route('/transcribe', methods=['POST', 'GET'])
 @cross_origin(supports_credentials=True)
 def run_model():
+    '''
+    Returns JSON of most-confident predictions made by text classification model.
+    Gets image name argument from url api call.
+    '''
+    #get image file name from url arguments
     args = request.args
-    imageName = args['name']
+    image_name = args['name']
 
     if request.method == 'GET':
-        # model = SequenceTagger.load_from_file('final-model.pt')#database.child("model/best-model.pt")
-        image = downloadImage(imageName)
-        #image = Image.open('000983.jpg')
-        scrape, readable = run_OCR(image)
+        image = download_image(image_name)
+        scrape, readable = run_ocr(image)
         finish = {}
         if readable:
-            model.predict(scrape)
+            #text classification model alters sentance by adding prediction tags 
+            TEXT_MODEL.predict(scrape)
             fields = []
             for span in scrape.get_spans('ner'):
                 fields.append(span.to_dict())
+            #gets most confident prediction for each field
             for dictionary in fields:
                 if dictionary['type'] in finish:
                     if dictionary['confidence'] > finish[dictionary['type']][1]:
                         finish[dictionary['type']] = [dictionary['text'], dictionary['confidence']]
                 else:
                     finish[dictionary['type']] = [dictionary['text'], dictionary['confidence']]
-
             return jsonify(finish)
         else:
-            return jsonify('Unable to read!')
-
+            finish = 'Unable to read!'
+            return jsonify(finish)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    APP.run(host='0.0.0.0', port=8000, debug=True)
