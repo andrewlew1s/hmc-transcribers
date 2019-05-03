3f6e21f3a0fa865f39ea345118a51b7f179aba85
Isaiah-Evans <ievans@hmc.edu>
2019-05-03 03:04:15 -0700

    Finished unit test for check_input

--- appTest.py
+++ appTest.py
@@ -11,12 +11,12 @@ class api_unit_tests(unittest.TestCase):
     testing set up here.
     """
 
-    def test_download_image(self):
+    def test_download_the_image(self):
         """
-        Test for download_image function
+        Test for download_the_image function
         """
         img_name = '000983.jpg'
-        return_val = app.download_image(img_name)
+        return_val = app.download_the_image(img_name)
         typeof_pic = PIL.JpegImagePlugin.JpegImageFile
         self.assertIsInstance(return_val, typeof_pic)
 
@@ -36,10 +36,13 @@ class api_unit_tests(unittest.TestCase):
         """
         Test for check_input function
         """
+        phone_sigs = ['cell', 'Cell', 'phone', 'Phone', 'Phone/fax', 'phone/fax', 'Phone/Fax']
+        fax_sigs = ['Fax', 'fax']
+        
         # Check for email address
         sentence = Sentence()
         token = Token('hello')
-        tag = 'email_id'
+        tag = 'S-email_id'
         token.add_tag('ner', tag)
         sentence.add_token(token)
         app.check_input(sentence)
@@ -47,12 +50,50 @@ class api_unit_tests(unittest.TestCase):
         self.assertNotEqual(return_val,tag)
 
         # Check for phone number
+        for sig in phone_sigs:
+            sentence = Sentence()
+            token = Token(sig)
+            tag = 'S-phone'
+            token.add_tag('ner', tag)
+            app.check_input(sentence)
+            return_val = sentence[0].get_tag('ner')
+            self.assertNotEqual(return_val,tag)
+
+            token = Token('123-456-7890')
+            app.check_input(sentence)
+            return_val = sentence[1].get_tag('ner')
+            self.assertEqual(return_val,tag)
+
 
         # Check for fax number
+        for sig in fax_sigs:
+            sentence = Sentence()
+            token = Token('Fax')
+            tag = 'S-fax'
+            token.add_tag('ner', tag)
+            app.check_input(sentence)
+            return_val = sentence[0].get_tag('ner')
+            self.assertNotEqual(return_val,tag)
+            
+            token = Token('123-456-7890')
+            app.check_input(sentence)
+            return_val = sentence[1].get_tag('ner')
+            self.assertEqual(return_val,tag)
 
         # Check for zipcode
-
-        # self.assertTrue(True)   # Placeholder
+        num = ''
+        for i in range(10):
+            num+=str(i)
+            sentence = Sentence()
+            token = Token('12345')
+            tag = 'S-zipcode'
+            token.add_tag('ner', tag)
+            app.check_input(sentence)
+            return_val = sentence[0].get_tag('ner')
+            if len(num) == 5:
+                self.assertEqual(return_val,tag)
+            else:
+                self.assertNotEqual(return_val,tag)        
 
 if __name__ == '__main__':
     unittest.main()
